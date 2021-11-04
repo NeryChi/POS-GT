220ce1852925f0a3af3ef803927425e986cd6df5
NeryChi <67949314+NeryChi@users.noreply.github.com>
2021-11-04 11:36:57 -0600

    Nery - Diseño de log-in.fxml

--- src/main/java/com/nerypolar/posgt/Main.java
+++ src/main/java/com/nerypolar/posgt/Main.java
@@ -3,6 +3,7 @@ package com.nerypolar.posgt;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("log-in.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Helloooo!");
+        stage.setTitle("");
         stage.setScene(scene);
         stage.show();
     }
