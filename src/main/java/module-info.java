0dddf2a54ddc35c1dd57fe11d2ee107799857b2c
NeryChi <67949314+NeryChi@users.noreply.github.com>
2021-11-09 00:56:37 -0600

    Nery - bibliotecas y diseno

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,8 @@
 module com.nerypolar.posgt {
-    requires javafx.graphics;
-    requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.java;
-
 
     opens com.nerypolar.posgt to javafx.fxml;
     exports com.nerypolar.posgt;
