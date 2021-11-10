dcd56df10dc7463c1d8925f5993801b82ba1879a
Polar502 <marioarita502@gmail.com>
2021-11-09 20:48:26 -0600

    home.fxml home.css

--- src/main/java/com/nerypolar/posgt/HomeCont.java
+++ src/main/java/com/nerypolar/posgt/HomeCont.java
@@ -1,16 +1,24 @@
 package com.nerypolar.posgt;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeCont implements Initializable {
 
+    @FXML
+    private ComboBox cmbProveedor;
 
+    @FXML
+    private ObservableList listaProveedores;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        cmbProveedor.setItems(listaProveedores);
 
     }
 }
