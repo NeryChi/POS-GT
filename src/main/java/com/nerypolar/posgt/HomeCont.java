f14fd43df925cfe383f420b04eb641f14e375840
NeryChi <67949314+NeryChi@users.noreply.github.com>
2021-11-09 22:46:44 -0600

    Nery - Modificacion a usuarios

--- src/main/java/com/nerypolar/posgt/HomeCont.java
+++ src/main/java/com/nerypolar/posgt/HomeCont.java
@@ -3,22 +3,70 @@ package com.nerypolar.posgt;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeCont implements Initializable {
 
-    @FXML
-    private ComboBox cmbProveedor;
+
+    //Panel izquierdo--------------------------------------------------------------------------------------------------
 
     @FXML
-    private ObservableList listaProveedores;
+    private TabPane madre;
+
+    /*@FXML
+    private Button btn_usr;*/
+
+
+    //Panel de usuarios -----------------------------------------------------------------------------------------------
+
+    @FXML
+    private Tab usuarios;
+
+   /*@FXML
+    private Button btn_agreg_usr;*/
+
+        //Panel agregar usuario----------------------------------------------------------------------------------------
+
+        @FXML
+        private Tab agregar_usuario;
+
+        @FXML
+        private TextField txf_nombre_usr;
+
+        /*@FXML
+        private Button btn_acept_usr;
+
+        @FXML
+        private Button btn_cancel_usr;*/
+
+    //-----------------------------------------------------------------------------------------------------------------
+
+
+
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        cmbProveedor.setItems(listaProveedores);
+
+
 
     }
+
+    public void irAggUsuario(){
+        madre.getSelectionModel().select(agregar_usuario);
+    }
+
+    public void aggUsuario(){
+
+    }
+
+    public void irUsuarios(){
+        txf_nombre_usr.setText("");
+        madre.getSelectionModel().select(usuarios);
+    }
+
 }
